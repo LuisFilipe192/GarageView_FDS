e9f0c67c7254577af18264210cf4c6f6d7525349
JoãoViniciusZ <joaovrdfreitas@gmail.com>
2026-03-30 20:11:41 -0300

    fix: limpar e recriar arquivos do forum com encoding correto

--- forum/models.py
+++ forum/models.py
@@ -1,6 +1,5 @@
-﻿from django.db import models
-import datetime
-from django.utils import timezone
+﻿# -*- coding: utf-8 -*-
+from django.db import models
 
 
 class Anuncio(models.Model):
@@ -9,7 +8,7 @@ class Anuncio(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     imagem_url = models.URLField(blank=True, null=True)
     vendedor = models.CharField(max_length=200, default='anônimo')
-    data_criacao = models.DateTimeField('Criado em ', auto_now_add=True)
+    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
 
     def __str__(self):
         return f'[{self.id}] {self.titulo} - {self.vendedor}'
