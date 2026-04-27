33ac329da5be9bae4b0887821f8924fed9b6cfba
LuisFilipe192 <LuisFilipe192@users.noreply.github.com>
2026-04-27 19:21:12 -0300

    adicionando codigo para as funcionalidades da aplicação

--- project/settings.py
+++ project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-change-this')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
